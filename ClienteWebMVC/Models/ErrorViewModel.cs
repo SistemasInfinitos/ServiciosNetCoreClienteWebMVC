56bbb00e4c9c2b4e72170719714522ab32e9f437
MAURICIO BELLO <bello900@hotmail.com>
2022-03-18 20:29:42 -0500

    inicio

---
+++ ClienteWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClienteWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
