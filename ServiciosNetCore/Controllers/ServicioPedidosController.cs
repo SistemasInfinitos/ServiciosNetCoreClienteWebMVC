ce7acc3967013073dcb1c5fdae6d249f3de2e0a5
MAURICIO BELLO <bello900@hotmail.com>
2022-03-19 13:01:35 -0500

    ServicioPedidosController

--- ServiciosNetCore/Controllers/ServicioPedidosController.cs
+++ ServiciosNetCore/Controllers/ServicioPedidosController.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiciosNetCore.Configuration;
+using ServiciosNetCore.ModelsAPI.Comun;
+using ServiciosNetCore.ModelsAPI.DataTable;
+using ServiciosNetCore.ModelsAPI.Pedidos;
+using ServiciosNetCore.ModelsDB.Contexts;
+using ServiciosNetCore.Repositorio.PedidosES;
+using ServiciosNetCore.Repositorio.ProcuctosES;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ServiciosNetCore.Controllers
@@ -11,5 +20,123 @@ namespace ServiciosNetCore.Controllers
     [ApiController]
     public class ServicioPedidosController : ControllerBase
     {
+        private readonly IPedidosESRepositorio _repositoryPedido;
+        public ServicioPedidosController(IOptionsMonitor<JwtConfiguracion> optionsMonitor, Context context)
+        {
+            _repositoryPedido = new PedidosESRepositorio(optionsMonitor, context);
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> CrearPedido([FromBody] EncabezadoPedidosModel entidad)
+        {
+            ResponseApp data = new ResponseApp()
+            {
+                mensaje = "Ups!. Tu Solicitud No Pudo ser Procesada",
+                ok = false
+            };
+            try
+            {
+                data.ok = await Task.Run(() => _repositoryPedido.CrearPedido(entidad));
+                data.mensaje = "Transaccion exitosa!";
+                return Ok(data);
+            }
+            catch (Exception x)
+            {
+                data.mensaje = "Ups!. Algo salio mal!. Error interno. " + x.HResult;
+                return BadRequest(data);
+            }
+        }
+
+        [Route("[action]")]
+        [HttpPut]
+        public async Task<IActionResult> ActualizarPedidoEncabezado([FromBody] EncabezadoPedidosModel entidad)
+        {
+            ResponseApp data = new ResponseApp()
+            {
+                mensaje = "Ups!. Tu Solicitud No Pudo ser Procesada",
+                ok = false
+            };
+            try
+            {
+                data.ok = await Task.Run(() => _repositoryPedido.ActualizarPedidoEncabezado(entidad));
+                data.mensaje = "Transaccion exitosa!";
+                return Ok(data);
+            }
+            catch (Exception x)
+            {
+                data.mensaje = "Ups!. Algo salio mal!. Error interno. " + x.HResult;
+                return BadRequest(data);
+            }
+        }
+
+        [Route("[action]")]
+        [HttpDelete]
+        public async Task<IActionResult> DeletePedido(int? id)
+        {
+            ResponseApp data = new ResponseApp()
+            {
+                mensaje = "Ups!. Tu Solicitud No Pudo ser Procesada",
+                ok = false
+            };
+
+            if (id != null)
+            {
+                try
+                {
+                    data.ok = await Task.Run(() => _repositoryPedido.DeletePedido(id.Value));
+                    data.mensaje = "Transaccion exitosa!";
+                    return Ok(data);
+                }
+                catch (Exception x)
+                {
+                    data.mensaje = "Ups!. Algo salio mal!. Error interno. " + x.HResult;
+                }
+            }
+            return BadRequest(data);
+        }
+
+        [Route("[action]", Name = "GetPedido")]
+        [HttpGet]
+        public async Task<IActionResult> GetPedido(string buscar, int? id)
+        {
+            bool ok = false;
+            string mensaje = "Sin Datos";
+            var pedido = await Task.Run(() => _repositoryPedido.GetPedido(buscar, id));
+            if (pedido != null && pedido.id > 0)
+            {
+                mensaje = "ok";
+                ok = true;
+            }
+            var options = new JsonSerializerOptions { IncludeFields = true };
+            var json = JsonSerializer.Serialize(pedido, options);
+            return Ok(json);
+        }
+
+        [Route("[action]", Name = "GetPedidoDropList")]
+        [HttpGet]
+        public async Task<IActionResult> GetPedidoDropList(string buscar, int? id)
+        {
+            var pedido = await Task.Run(() => _repositoryPedido.GetPedidosDropList(buscar, id));
+            return Ok(pedido);
+        }
+
+        /// <summary>
+        /// Metodo post espesial para Datatable
+        /// </summary>
+        /// <param name="dtParms"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> ListPedidos([FromBody] DataTableParameter datatParms)
+        {
+            DataTableResponsePedido res = await Task.Run(() => _repositoryPedido.GetPedidosDataTable(datatParms));
+            res.draw = datatParms.draw;
+
+            var options = new JsonSerializerOptions { IncludeFields = true };
+            var json = JsonSerializer.Serialize(res, options);
+
+            return Ok(json);
+        }
     }
 }
