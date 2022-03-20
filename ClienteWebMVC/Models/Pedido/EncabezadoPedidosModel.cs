0ad8214017d837b4547ac22b6cebe3f75a802758
MAURICIO BELLO <bello900@hotmail.com>
2022-03-20 17:39:36 -0500

    inicio diseños pedido

--- ClienteWebMVC/Models/Pedido/EncabezadoPedidosModel.cs
+++ ClienteWebMVC/Models/Pedido/EncabezadoPedidosModel.cs
@@ -7,10 +7,10 @@ namespace ClienteWebMVC.Models.Pedido
         public int? id { get; set; }
         public int? usuarioId { get; set; }
         public int clientePersonaId { get; set; }
-        public decimal valorNeto { get; set; }
-        public decimal valorIva { get; set; }
-        public decimal valorTotal { get; set; }
-        public bool? estado { get; set; }
+        public decimal? valorNeto { get; set; }
+        public decimal? valorIva { get; set; }
+        public decimal? valorTotal { get; set; }
+        public bool estado { get; set; }
         public string fechaCreacion { get; set; }
         public string fechaActualizacion { get; set; }
 
