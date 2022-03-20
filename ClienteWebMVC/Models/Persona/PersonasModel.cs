bd0b8e1543f5d7cc74a5b2213ebe43e12bcf092e
MAURICIO BELLO <bello900@hotmail.com>
2022-03-19 20:12:07 -0500

    PersonasModel

--- ClienteWebMVC/Models/Persona/PersonasModel.cs
+++ ClienteWebMVC/Models/Persona/PersonasModel.cs
@@ -3,33 +3,14 @@ namespace ClienteWebMVC.Models.Persona
 {
     public class PersonasModel
     {
-        public int id { get; set; }
+        public int? id { get; set; }
         [Required]
-        public string identificacion { get; set; }
+        public string nombres { get; set; }
         [Required]
-        public string primerNombre { get; set; }
-        public string segundoNombre { get; set; }
-        [Required]
-        public string primerApellido { get; set; }
-        public string segundoApellido { get; set; }
-        [Required]
-        public string telefono { get; set; }
-        [Required]
-        public string correo { get; set; }
-
-        [Required]
-        public int idCiudad { get; set; }
-        [Required]
-        public int edad { get; set; }
+        public string apellidos { get; set; }
         public string fechaCreacion { get; set; }
         public string fechaActualizacion { get; set; }
         public bool estado { get; set; }
-
-        public virtual int idDepartamento { get; set; }
-
-        /// <summary>
-        /// nombre combinado de la persona
-        /// </summary>
         public virtual string cliente { get; set; }
     }
 }
