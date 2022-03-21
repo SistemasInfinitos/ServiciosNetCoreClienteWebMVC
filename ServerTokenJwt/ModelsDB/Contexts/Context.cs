2278d5dc77e44ccc46d14f8faac69d9808a0e2db
MAURICIO BELLO <bello900@hotmail.com>
2022-03-21 13:51:29 -0500

    Scaffold-DbContext "Server=SISTEMASINFINIT\SERVERDESARROLLO;Database=DesarrolladorSemiSenior;user=sa;Password=YoSoyJovenDeCristo*;trustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir ModelsDB -ContextDir ModelsDB\Contexts -Context Context -UseDatabaseNames -Tables Usuarios,Personas -force

--- ServerTokenJwt/ModelsDB/Contexts/Context.cs
+++ ServerTokenJwt/ModelsDB/Contexts/Context.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
-using ServerTokenJwt.ModelsDB;
+﻿using Microsoft.EntityFrameworkCore;
 
 #nullable disable
 
